e2581f879fdd3fac38b7da38aee489c249187f42
Holtom <g8s8yanghaotao@yujing.live>
2025-03-10 23:34:19 +0800

    上传打开目录功能

---
+++ pages/upload/upload.js
@@ -0,0 +1,23 @@
+Page({
+  data: {},
+
+  chooseImage() {
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image'],
+      sourceType: ['album'],
+      success: (res) => {
+        const tempFilePath = res.tempFiles[0].tempFilePath;
+        console.log('选择的图片路径:', tempFilePath);
+        // 这里可以添加后续的图片处理逻辑
+      },
+      fail: (err) => {
+        console.error('选择图片失败:', err);
+        wx.showToast({
+          title: '选择图片失败',
+          icon: 'none'
+        });
+      }
+    });
+  }
+}); 
